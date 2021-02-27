ad0cb9d4a0e3ec6abfeed3407bd63ee6cca6f223
BuildTools <unconfigured@null.spigotmc.org>
2021-02-27 10:03:51 +0300

    Итоговая версия 0.9v (Предфинальная)

--- Виселица.cpp
+++ Виселица.cpp
@@ -12,8 +12,9 @@ using namespace std;
 	3. Загадывается слово для игры
 	4. Начало игры. Визуальное представление о слове
 	5. Ввод попыток игрока
-	6. Результат (Победа/поражение)
-	7. Статистика
+	6. Результат (Победа/поражение) (todo)
+	7. Статистика (todo)
+	8. Выбор уровня сложности (todo)
 */
 
 void welcomeMessage()
@@ -22,19 +23,26 @@ void welcomeMessage()
 	cout << "* Добро пожаловать в игру Виселица!\n";
 	cout << "* \n";
 	cout << "* Вам предстоит угадать загаданное слово за 10 попыток,\n";
-	cout << "* После чего игра будет окончена!\n";
+	cout << "* После чего игра будет окончена! (В данной версии попыток неограниченно!)\n";
+	cout << "*\n";
+	cout << "* Если вы захотите досрочно завершить игру, то введите следующий символ - 0 (нолик)\n";
+	cout << "*\n";
+	cout << "* Правила:";
+	cout << "* Дано случайно слово, которое побуквенно отображается у игрока на экране\n";
+	cout << "* С каждой угаданной буквой будет уменьшаться все неизвестные\n";
+	cout << "* Условие победы выполнится тогда, когда все буквы будут угаданы!\n";
 	cout << "* \n";
 	cout << "* Удачи и веселой игры!\n";
 	cout << "* * * * * * * * * * * * * * * * * * * *\n";
 }
 
-string inputName()
-{
-	cout << "Введите ваше имя: ";
-	string name;
-	getline(cin, name);
-	return name;
-}
+//string inputName()
+//{
+//	cout << "Введите ваше имя: ";
+//	string name;
+//	getline(cin, name);
+//	return name;
+//}
 
 string choiceWord()
 {
@@ -67,14 +75,16 @@ void game()
 			cout << element << ' ';
 	cout << "\n";
 
+	// Пока количество символов в угадываемом слове будет больше 0
 	while (remainingLetters > 0)
 	{
 		cin >> inputUser;
+		cout << "\n";
 
 		// Проверка пользовательского ввода
 		if (inputUser.size() > 1)
 		{
-			cout << "ОШИБКА!\n";
+			cout << "\033[31;1mОШИБКА! Нельзя вводить больше 1 символа!\nПовторите ввод!\n \033[0m";
 		}
 		else if (inputUser == nolik)
 		{
@@ -91,27 +101,24 @@ void game()
 					for (auto element : answerArray)
 						cout << element << ' ';
 					remainingLetters--;
-					cout << remainingLetters << " <- количество символов оствлось\n";
 				}
 			}
 		}
 	}
 
-
-	cout << "ФУНКЦИЯ GAME: " << currentWord;
+	cout << "\n";
+	cout << "Вы победили! Было выбранно слово: \033[32;1m" << currentWord << "\n \033[0m";
 }
 
 int main()
 {
-	// setlocale(LC_ALL, "russian");
 	// Кодировка кириллицы для консоли
+	setlocale(LC_ALL, "russian");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	//
 	srand(static_cast<unsigned int>(time(0)));
-	//string name = inputName();
-	//cout << name << endl;
-	//welcomeMessage();
+	welcomeMessage();
 	game();
 	return 0;
 }
